74fc875d13fe20556257b47d18ee7e24400e999c
LuoAnbing <luoanb@163.com>
2023-02-22 14:33:11 +0800

    更新

--- apps/react-template/src/App.tsx
+++ apps/react-template/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import xx from "typescript-template"
 // import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState("你好");
-
+  useEffect(()=>{
+    console.log("xx",xx);
+  },[])
   return (
     <div className="App">
     </div>
