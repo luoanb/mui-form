e89ca821021333de5a494414e91bf31104ad2099
LAB <luoanb@163.com>
2023-03-13 16:14:10 +0800

    新增资源库

---
+++ packages/mui-layout/externalModule.js
@@ -0,0 +1,2 @@
+const external = ['react', 'react-dom', 'react-hook-form', '@mui/icons-material', '@mui/material', '@mui/x-data-grid', '@mui/lab']
+export default external
---
+++ packages/mui-layout/src/index.tsx
@@ -0,0 +1,9 @@
+interface Person {
+  name: string
+  age: number
+}
+const xiaoming: Person = {
+  name: '小明',
+  age: 17
+}
+export { xiaoming }
