17aafb3dd0f71583b1626e8436897118a37f7e77
LAB <luoanb@163.com>
2023-03-14 00:30:46 +0800

    新增defineComponent

---
+++ apps/mui-template/src/defineComponent.tsx
@@ -0,0 +1,26 @@
+import { forwardRef, useImperativeHandle } from "react";
+/**
+ * 组件返回
+ */
+export type RBase = {
+  element: JSX.Element;
+};
+/**
+ * 组件
+ */
+export type Component<P = any, R extends RBase = RBase> = (props: P) => R;
+
+/**
+ * 快速定义带外抛属性组件的高阶组件工具
+ * @param {Component} component 组件
+ * @returns React组件
+ */
+export function defineComponent<P = any, R extends RBase = RBase>(
+  component: Component<P, R>
+) {
+  return forwardRef<Omit<R, "element">, P>((props, ref) => {
+    const { element, ...attr } = component(props);
+    useImperativeHandle(ref, () => attr);
+    return element;
+  });
+}
--- apps/mui-template/vite.config.ts
+++ apps/mui-template/vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
     esbuildOptions: {
       loader: {
         '.js': 'jsx',
-        '.png': "file"
       },
     }
   },
