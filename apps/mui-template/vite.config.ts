185f4edcaaa3839f1e35c07932271d0b794017d1
LAB <luoanb@163.com>
2023-03-26 23:42:20 +0800

    更新打包设置

--- apps/mui-template/vite.config.ts
+++ apps/mui-template/vite.config.ts
@@ -1,20 +1,31 @@
-import path from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgPlugin from "./svgconfig"
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgPlugin from "./svgconfig";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgPlugin],
   base: "./",
+  build: {
+    commonjsOptions: {
+      include: [
+        /node_modules/,
+        /mui-form-hook/,
+        /ref-component/,
+        /mui-form-hook/,
+      ],
+      defaultIsModuleExports: "auto",
+    },
+  },
   esbuild: {
-    loader: 'tsx',
+    loader: "tsx",
     include: [/src\/.*\.[tj]sx?$/, /src\/.*\.png?$/],
   },
   optimizeDeps: {
     include: [
-      "mui-form-hook",
       "mui-layout-component",
       "ref-component",
+      "mui-form-hook",
       // "react",
     ],
     // include: ['/src'],
@@ -24,4 +35,4 @@ export default defineConfig({
     //   },
     // }
   },
-})
+});
