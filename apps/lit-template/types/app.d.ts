8437549e8c3262e7041cb0835ad14ebc5f508e85
LAB <luoanb@163.com>
2023-04-02 23:47:44 +0800

    更新

---
+++ apps/lit-template/types/app.d.ts
@@ -0,0 +1,7 @@
+import "@material/web/button/outlined-button";
+import { LitElement } from "lit";
+import "./my-element";
+export declare class App extends LitElement {
+    private router;
+    render(): import("lit").TemplateResult<1>;
+}
