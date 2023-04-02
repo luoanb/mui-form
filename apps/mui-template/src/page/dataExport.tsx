6bdc102c6598be68595daff326c3b55e540b8f21
LAB <luoanb@163.com>
2023-04-02 16:02:33 +0800

    新增更多导出格式;新增打印

--- apps/mui-template/src/page/dataExport.tsx
+++ apps/mui-template/src/page/dataExport.tsx
@@ -1,11 +1,77 @@
-import { Box, Card, CardHeader, IconButton, Tooltip } from "@mui/material";
+import { ComponentRef, useCallback, useMemo, useRef } from "react";
+import {
+  Box,
+  Card,
+  CardHeader,
+  IconButton,
+  MenuItem,
+  Stack,
+  Tooltip,
+} from "@mui/material";
 import { Export, FileExcel, TextBox } from "mdi-material-ui";
-import { MuiDataGrid } from "mui-form-hook";
+import { DropDown, MuiDataGrid } from "mui-form-hook";
 import { defineComponent } from "ref-component";
 import { UserList } from "../mock/user";
 import * as XLSX from "xlsx";
-import { GridColumns } from "@mui/x-data-grid";
-import { useCallback, useMemo } from "react";
+import { DataGrid, GridColumns } from "@mui/x-data-grid";
+import Print from "@mui/icons-material/Print";
+import MenuUp from "mdi-material-ui/MenuUp";
+import DotsVertical from "mdi-material-ui/DotsVertical";
+import { silentPrint } from "../utils/print";
+
+const bookTypes: XLSX.BookType[] = [
+  "xlsx",
+  "xlsm",
+  "xlsb",
+  "xls",
+  "xla",
+  "biff8",
+  "biff5",
+  "biff2",
+  "xlml",
+  "ods",
+  "fods",
+  "csv",
+  "txt",
+  "sylk",
+  "slk",
+  "html",
+  "dif",
+  "rtf",
+  "prn",
+  "eth",
+  "dbf",
+  "numbers",
+];
+
+const exportMore = (keys: any[]) => {
+  return (
+    <DropDown
+      target={({ open, handleOpen }) => (
+        <Tooltip title="更多导出">
+          <IconButton onClick={handleOpen}>
+            <DotsVertical />
+          </IconButton>
+        </Tooltip>
+      )}
+      downContent={({ handleClose }) => (
+        <Stack sx={{ p: 1 }}>
+          {keys.map((option) => (
+            <MenuItem
+              key={option.key}
+              onClick={() => {
+                option.onClick();
+                handleClose();
+              }}
+            >
+              {option.key}
+            </MenuItem>
+          ))}
+        </Stack>
+      )}
+    />
+  );
+};
 
 const columns1: GridColumns = [
   { field: "id", headerName: "ID" },
@@ -36,6 +102,7 @@ const columns1: GridColumns = [
 
 export default defineComponent(() => {
   const userList = useMemo(() => UserList(35), []);
+  const dataref = useRef<ComponentRef<typeof DataGrid>>(null);
   /**
    * 数据导出
    */
@@ -53,7 +120,7 @@ export default defineComponent(() => {
       });
       let wb = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(wb, ws, "页一");
-      XLSX.writeFile(wb, "导出文件.csv", { bookType: "csv" });
+      XLSX.writeFile(wb, "导出文件." + bookType, { bookType: "csv" });
     },
     []
   );
@@ -64,6 +131,23 @@ export default defineComponent(() => {
           title="数据导出"
           action={
             <>
+              <Tooltip title="打印">
+                <IconButton
+                  size="small"
+                  onClick={() => {
+                    if (dataref.current) {
+                      // document
+                      const printNode = dataref.current.cloneNode(
+                        true
+                      ) as HTMLElement;
+                      printNode.style.height = 150 * userList.length + "px";
+                      silentPrint(printNode?.outerHTML || "");
+                    }
+                  }}
+                >
+                  <Print />
+                </IconButton>
+              </Tooltip>
               <Tooltip title="Xlsx导出">
                 <IconButton
                   size="small"
@@ -80,10 +164,17 @@ export default defineComponent(() => {
                   <TextBox />
                 </IconButton>
               </Tooltip>
+              {exportMore(
+                bookTypes.map((key) => ({
+                  key,
+                  onClick: () => doExport(userList, key),
+                }))
+              )}
             </>
           }
         />
-        <MuiDataGrid
+        <DataGrid
+          ref={dataref}
           style={{ height: "100%" }}
           rowHeight={150}
           columns={columns1}
